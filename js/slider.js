5a8286c637a263eb6f1b72aab2206c880557a01b
IamCooller <45273146+IamCooller@users.noreply.github.com>
2020-06-23 18:42:25 +0300

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -73,3 +73,12 @@ function prevSlide() {
     slideNow--;
   }
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar_style") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar_style";
+  }
+}
